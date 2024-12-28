1fb82381a47ff510a4b9b6bd89b808b7000a7f68
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-28 18:24:48 +0530

    first commit

---
+++ 1.If.js
@@ -0,0 +1,5 @@
+let a = 20;
+if (a > 10)
+     {
+        console.log("value of \"a\" is greater than 10");
+     }  
---
+++ 2.find the grade.js
@@ -0,0 +1,25 @@
+let score=prompt("Enter Your Score 0 to 100");
+if(score>=90 && score<100)
+{
+    console.log("Grade A");
+}
+
+if(score>=80 && score<90)
+{
+    console.log("Grade B");
+}
+
+if(score>=70 && score<79)
+{
+    console.log("Grade C");
+}
+
+if(score>=60 && score<69)
+{
+    console.log("Grade D");
+}
+
+if(score<60)
+{
+    console.log("You are Fail");
+}
