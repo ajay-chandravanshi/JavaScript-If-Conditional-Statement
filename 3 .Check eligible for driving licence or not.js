bf2a8ba1de35264c9ba5e4b3385e563b2931939f
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-30 23:27:19 +0530

    new code

---
+++ 3 .Check eligible for driving licence or not.js
@@ -0,0 +1,9 @@
+let age=prompt("Enter your age");
+    if(age<18)
+    {
+        alert("You Are Not Eligible for driving licence");
+    }
+    if(age>=18)
+    {
+        alert("You Are Eligible for driving licence");
+    }
---
+++ 4.Find greatest number.js
@@ -0,0 +1,14 @@
+    let first=prompt("Enter the First Number");
+    let second=prompt("Enter the First Number");
+    if(first>second)
+    {
+        alert("First number is greater then Second");
+    }
+    if(first<second)
+    {
+        alert("Second number is greater then First");
+    }
+    if(first==second)
+    {
+        alert("Both Number is Equal");
+    }
